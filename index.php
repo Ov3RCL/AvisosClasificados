aeab459ceafea40744d7bf17107930f2e54b1f92
Ov3R <ov3rcl@gmail.com>
2020-09-08 13:23:27 -0300

    Se agregaron las ventanas modales al proyecto

--- index.php
+++ index.php
@@ -23,17 +23,22 @@
             <li class="nav-item active">
                 <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Registro</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Entrar</a>
-            </li>
+           
             </ul>
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Buscar Aviso..." aria-label="Search">
                 <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Buscar</button>
             </form>
+
+            <ul class="navbar-nav ml-1">
+                <li class="nav-item ml-1">
+                    <button class="btn btn-sm btn-info" data-toggle="modal" data-target="#modalRegistrarse">Registrar</button>
+                </li>
+                <li class="nav-item ml-1">
+                    <button class="btn btn-sm btn-secondary" type="button" data-toggle="modal" data-target="#modalInicioSesion">Entrar</button>
+                </li>
+            </ul>
+
         </div>
     </nav>  
     <!-- fin navbar -->
@@ -44,6 +49,7 @@
                   <!-- menu categorias -->
                   <p><i class="fas fa-box-open"></i> Categorias</p>
                 <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action">Todos</a>
                     <a href="#" class="list-group-item list-group-item-action">Automoviles</a>
                     <a href="#" class="list-group-item list-group-item-action">Computadores</a>
                     <a href="#" class="list-group-item list-group-item-action">Servicios</a>
@@ -67,7 +73,8 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Titulo del Aviso</h5>
                                     <p class="card-text">Descripcion del aviso ... </p>
-                                    <a href="#" class="btn btn-primary">Informacion</a>
+                                    <p class="card-text"><i class="fas fa-hand-holding-usd"></i> 10.000 </p>
+                                    <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#modalInformacionUsuario" >Informacion</a>
                                 </div>
                             </div>
                         
@@ -80,7 +87,8 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Titulo del Aviso</h5>
                                     <p class="card-text">Descripcion del aviso ... </p>
-                                    <a href="#" class="btn btn-primary">Informacion</a>
+                                    <p class="card-text"><i class="fas fa-hand-holding-usd"></i> 10.000 </p>
+                                    <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#modalInformacionUsuario">Informacion</a>
                                 </div>
                             </div>
                         
@@ -98,12 +106,8 @@
     </div>
 
 
-  
 
-
-
-
-
+    <?php require_once('modales.php'); ?>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
